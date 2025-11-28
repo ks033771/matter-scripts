b3d496cbd6241b1ed2abdf519991fbac13c08ca7
ks033771 <115696773+ks033771@users.noreply.github.com>
2025-11-28 12:52:50 +0100

    Change TOP_WALL_OFFSET to 0

--- matter-v1.js
+++ matter-v1.js
@@ -10,7 +10,7 @@
 
 /* ---------- Konfiguration ---------- */
 const THICCNESS = 120;
-const TOP_WALL_OFFSET = 660;
+const TOP_WALL_OFFSET = 0;
 const SVG_PATH_SELECTOR = "#matter-path";
 const SVG_WIDTH_IN_PX = 500;
 const SVG_WIDTH_AS_PERCENT_OF_CONTAINER_WIDTH = 0.30;
