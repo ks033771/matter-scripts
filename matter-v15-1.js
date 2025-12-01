336f6427093accefab884196888e919cb54353b8
ks033771 <115696773+ks033771@users.noreply.github.com>
2025-12-01 16:32:56 +0100

    Update shape counts in matter-v15-1.js

--- matter-v15-1.js
+++ matter-v15-1.js
@@ -243,10 +243,10 @@ function resolvePaintFromVars(def, refEl = matterContainer || document.documentE
 /* ---------- SVG-Defs inline (anstatt echter <svg>-Elemente) ---------- */
 /* Mehrfache Instanzen pro Path */
 const SHAPE_COUNTS = {
-  "#matter-shape-1": 5,
-  "#matter-shape-2": 5,
-  "#matter-shape-3": 5,
-  "#matter-shape-4": 5
+  "#matter-shape-1": 7,
+  "#matter-shape-2": 7,
+  "#matter-shape-3": 7,
+  "#matter-shape-4": 7
 };
 
 /**
